e7a4677c850d2f765b96b020b49add3444692826
LaToya Green <latoyargreen@hotmail.com>
2015-08-20 21:37:24 -0500

    New project

---
+++ Verve/StaticTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  StaticTableViewController.h
+//  Verve
+//
+//  Created by Verve on 8/20/15.
+//  Copyright (c) 2015 MyLife. All rights reserved.
+//
+
+#ifndef Verve_StaticTableViewController_h
+#define Verve_StaticTableViewController_h
+
+
+#endif
---
+++ Verve/StaticTableViewController.m
@@ -0,0 +1,9 @@
+//
+//  StaticTableViewController.m
+//  Verve
+//
+//  Created by Verve on 8/20/15.
+//  Copyright (c) 2015 MyLife. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
